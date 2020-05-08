65f8424f760d8bb5d6b4dbb8e1d01303562d4e42
mtsalka <maryia.miskevich@gmail.com>
2020-05-08 11:52:28 +0300

    feat: added transform arrays task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,38 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+    if (Array.isArray(arr)) {
+      let controlers = new Set(['--discard-next', '--double-next', '--double-prev', '--double-prev'])
+      for (let i = 0; i < arr.length; i++) {
+        switch (arr[i]) {
+          case '--double-next':
+            if (i < arr.length - 1 && !controlers.has(arr[i+1])) {
+              arr.splice(i, 1, arr[i+1]);
+              i++;
+            } else arr.splice(i, 1);
+          break;
+          case '--double-prev':
+            if (i > 0 && !controlers.has(arr[i-1])) {
+              arr.splice(i, 1, arr[i-1]);
+              i++;
+            } else arr.splice(i, 1);
+          break;
+        }
+      }
+      for (let i = 0; i < arr.length; i++) {
+        switch (arr[i]) {
+          case '--discard-next':
+            if (i < arr.length - 1 && !controlers.has(arr[i+1])) {
+              arr.splice(i, 2);
+              i--;
+            } else arr.splice(i, 1);
+          break;
+          case '--discard-prev':
+            if (i > 0 && !controlers.has(arr[i-1])) {
+              arr.splice(i-1, 2);
+              i--;
+            } else arr.splice(i, 1);
+          break;
+        }
+      }
+      return arr;
+    } else trow(new Error);
+  }
