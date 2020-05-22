8f0aa47c074f0599cc86e666ebe25c6ddd99e50c
mtsalka <maryia.miskevich@gmail.com>
2020-05-22 17:31:52 +0300

    feat: added vigenere and what season

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,52 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+
+    constructor (isDirect) {
+      this.isDirect = isDirect === false? false : true;
+    }
+    encrypt(message, key) {
+      if (message && key) {
+
+        if (!this.isDirect) {
+          message = message.split('').reverse().join('');
+        }
+        let encryptedMessage = '';
+        key = key.toUpperCase();
+        message = message.toUpperCase();
+        let keyPosition = 0;
+        for (let i=0; i < message.length; i++) {
+          if (message.codePointAt(i) > 64 && message.codePointAt(i) < 91) {
+            encryptedMessage += String.fromCodePoint(((message.codePointAt(i) - 65) + (key.codePointAt(keyPosition % key.length) - 65)) % 26 + 65);
+            keyPosition++;
+          } else {
+            encryptedMessage += message[i];
+          }
+        }
+        return encryptedMessage;
+      } else trow (new Error);
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    decrypt(encryptedMessage, key) {
+      if (encryptedMessage && key) {
+        
+        if (!this.isDirect) {
+          encryptedMessage = encryptedMessage.split('').reverse().join('');
+        }
+
+        let message = '';
+        key = key.toUpperCase();
+        encryptedMessage = encryptedMessage.toUpperCase();
+        let keyPosition = 0;
+        for (let i=0; i < encryptedMessage.length; i++) {
+          if (encryptedMessage.codePointAt(i) > 64 && encryptedMessage.codePointAt(i) < 91) {
+            message += String.fromCodePoint(((encryptedMessage.codePointAt(i) - 65) + 26 - (key.codePointAt(keyPosition % key.length) - 65)) % 26 + 65);
+            keyPosition++;
+          } else {
+            message += encryptedMessage[i];
+          }
+        }
+        // if (!this.isDirect) message.split('').reverse().join('');
+        return message;
+      } else trow (new Error);
     }
 }
 
--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,14 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  const SEASONS = [
+    'winter',
+    'spring',
+    'summer',
+    'fall'
+  ];
+  if (!date) return 'Unable to determine the time of year!'; 
+  if (date instanceof Date && !date.hasOwnProperty('getMonth')) {
+    let mount = date.getMonth() + 2;
+    if (mount === 13) mount = 1;
+    return SEASONS[Math.ceil(mount/3) - 1];
+  }else throw new Error();
 };
